b246f458f7f9a819eafa0a35a7bd514752026a45
fidelscodes <fidelscodes@gmail.com>
2016-08-05 11:08:17 -0400

    Done

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 class Application < Sinatra::Base
-  
+  # route to respond to the homepage
   get '/' do
     erb :index
   end
 
+  # route to responde to the form submission
   post '/greet' do
     erb :greet
   end
