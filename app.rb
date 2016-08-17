27bbc5eb25b7dfc019f4c7ebe136cfe755fb2ea3
hafizrahman1 <hafiz.bd2009@gmail.com>
2016-08-17 16:48:53 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 class Application < Sinatra::Base
-  
+  # Old route from previous tests
   get '/' do
     erb :index
   end
-
+  # New route to respond to the form submission
   post '/greet' do
     erb :greet
   end
