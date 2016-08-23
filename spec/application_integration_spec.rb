1cfab3413ee7e5dff45c7a599d31ca3984b73695
Dmitriy Khait <khaitdmitriy@gmail.com>
2016-08-23 10:22:43 -0400

    Done.

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -1,1 +1,10 @@
 # Write your tests here!
+
+# require 'spec_helper'
+#
+# describe "GET '/' - Greeting Form" do
+#   it 'welcomes the user' do
+#     visit '/'
+#     expect(page.body).to include("Welcome!")
+#   end
+# end
